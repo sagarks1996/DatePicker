71aa63ee10b9f2e405b2b4d98a003a66c425aecc
amaljoy <amal.joy.tk@gmail.com>
2019-11-12 15:49:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    Button button;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = findViewById(R.id.datePicker_id);
+        button = findViewById(R.id.submit);
+        textView = findViewById(R.id.textview_id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = datePicker.getDayOfMonth();
+                int month = datePicker.getMonth();
+                int year = datePicker.getYear();
+
+                textView.setText(day+":"+month+":"+year);
+            }
+        });
+    }
+}
